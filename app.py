b3a1e3ec762c6d39d06f7b083d22b3b97573d861
staniher <staniher@yahoo.fr>
2025-03-26 12:46:07 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, render_template
+import joblib
+#Chargement du modele
+modele=joblib.load('modeleDT.pkl')
+#Instanciation de Flask
+app=Flask(__name__)
+#Debut des APIs
+@app.route('/')
+def home():
+	return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+	mesFeatures=[float(x) for x in request.form.values()]
+	import numpy as np
+
+	mesFeatures=[np.array(mesFeatures)]
+	resultat=round(modele.predict(mesFeatures)[0],2)
+	res=f"La charge d'assurance à payer vaut: {resultat}$"
+	return render_template('index.html', 
+		resultat_prediction=res)
+
+
+
+if __name__=="__main__":
+	app.run(debug=True)
